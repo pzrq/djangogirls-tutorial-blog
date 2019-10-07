c1bbcd0706c5ae334d133762277ecab4739b5d1f
Peter Schmidt <peter@peterjs.com>
2019-10-07 18:08:17 +1100

    🔓🙈 Publish a SECRET_KEY
    I fold (for now at least).
    
    Supporting secure out of the box would be nice, but I can also see it's close to the too-hard basket for people who're just volunteering and not renumerated (it is the kind of thing that becomes an issue as soon as one goes past the just trying it out stage though):
    https://help.pythonanywhere.com/pages/environment-variables-for-web-apps
    https://stackoverflow.com/questions/15170637/effects-of-changing-djangos-secret-key/15383766
    https://github.com/DjangoGirls/tutorial-extensions/issues/101

--- myponysite/settings.py
+++ myponysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "yeg-*ht$p#f%w)kz498c3%6c268wkpa%cza74r56h(zj&oy^ws"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
